bd62579b587c7c580436bcef2df245f62b77386b
Daniel Hsu <danieljhsu@gmail.com>
2015-07-21 16:28:59 -0700

    first commit

---
+++ Bumptail/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Bumptail
+//
+//  Created by Daniel Hsu on 7/21/15.
+//  Copyright (c) 2015 Daniel Hsu. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var discoverButton: UIButton!
+    @IBOutlet weak var meButton: UIButton!
+    @IBOutlet weak var createButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
